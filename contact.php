a3ba433c016cb84cf10d65b06c6ad16b1bc386b1
dgalvarez <dgalvarez2020@gmail.com>
2020-11-03 00:04:44 -0400

    sistema hospitalario

---
+++ contact.php
@@ -0,0 +1,113 @@
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='contact.php'</script>";
+
+}
+
+
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>HOSPITAL SISTEMTAIPE | 2020</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
+		<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<!--start-wrap-->
+		
+			<!--start-header-->
+			<div class="header">
+				<div class="wrap">
+				<!--start-logo-->
+				<div class="logo">
+		<a href="index.html" style="font-size: 30px;">Hospital SISTEMATAIPE CHONTA 2020</a> 
+				</div>
+				<!--end-logo-->
+				<!--start-top-nav-->
+				<div class="top-nav">
+					<ul>
+						<li><a href="index.html">INICIO</a></li>
+					
+						<li class="active"><a href="contact.php">CONTACTO</a></li>
+					</ul>					
+				</div>
+				<div class="clear"> </div>
+				<!--end-top-nav-->
+			</div>
+			<!--end-header-->
+		</div>
+		    <div class="clear"> </div>
+		   <div class="wrap">
+		   	<div class="contact">
+		   	<div class="section group">				
+				<div class="col span_1_of_3">
+					
+      			<div class="company_address">
+				     	<h2>Direccion Hospital:</h2>
+						    	<p>500 Lorem Ipsum Dolor Sit,</p>
+						   		<p>22-56-2-9 Sit Amet, Lorem,</p>
+						   		<p>India</p>
+				   		<p>Telefono:(00) 222 666 444</p>
+				   		<p>Ruc: (000) 000 00 00 0</p>
+				 	 	<p>Email: <span>sistemataipe@hotmail.com.com</span></p>
+				   	
+				   </div>
+				</div>				
+				<div class="col span_2_of_3">
+				  <div class="contact-form">
+				  	<h2>Contacto</h2>
+					    <form name="contactus" method="post">
+					    	<div>
+						    	<span><label>NOMBRE</label></span>
+						    	<span><input type="text" name="fullname" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>E-MAIL</label></span>
+						    	<span><input type="email" name="emailid" required="ture" value=""></span>
+						    </div>
+						    <div>
+						     	<span><label>TELEFONO</label></span>
+						    	<span><input type="text" name="mobileno" required="true" value=""></span>
+						    </div>
+						    <div>
+						    	<span><label>DESCRIPCION</label></span>
+						    	<span><textarea name="description" required="true"> </textarea></span>
+						    </div>
+						   <div>
+						   		<span><input type="submit" name="submit" value="Submit"></span>
+						  </div>
+					    </form>
+				    </div>
+  				</div>				
+			  </div>
+			  	 <div class="clear"> </div>
+	</div>
+	<div class="clear"> </div>
+			</div>
+	      <div class="clear"> </div>
+		   <div class="footer">
+		   	 <div class="wrap">
+		   	<div class="footer-left">
+		   			<ul>
+						<li><a href="index.html">INICIO</a></li>
+						
+						<li><a href="contact.php">CONTACTO</a></li>
+					</ul>
+		   	</div>
+		  
+		   	<div class="clear"> </div>
+		   </div>
+		   </div>
+		<!--end-wrap-->
+	</body>
+</html>
+
